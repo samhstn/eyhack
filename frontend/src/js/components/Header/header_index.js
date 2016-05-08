d2c1f4cff2b9b415a53aab3a1040c06ad37be975
shouston3 <shouston3@me.com>
2016-05-08 13:37:49 +0100

    everything is styled to look like child expert physio

--- frontend/src/js/components/Header/header_index.js
+++ frontend/src/js/components/Header/header_index.js
@@ -44,5 +44,5 @@ export default class Header extends React.Component {
 }
 
 Header.defaultProps = {
-  menuItems: [ 'about', 'contact' ]
+  menuItems: [ 'about', 'blog', 'resources', 'contact', 'faq' ]
 }
--- frontend/src/js/views/About/about_index.js
+++ frontend/src/js/views/About/about_index.js
@@ -3,7 +3,19 @@ import React from 'react'
 export default () => {
   return (
     <div className='about'>
-      <h3>Put something about your app here</h3>
+      <h4>What is Child Expert Physio</h4>
+      <p>Child Expert Physio Facebook page is all about raising awareness of the possibilities for recovery following brain damage.</p>
+      <p>It features stories of hope and free information resources.</p>
+      <p>Child Expert Physio has aspirations to reach a global audience with it's educational programme aimed at parents, carers and professionals of children with brain damage</p>
+      <p>or any kind of condition affecting their ability to control their own body movement.</p>
+      <h4>Mission</h4>
+      <p >We exist to challenge the status quo in understanding of what is possible for a ​Human Being to achieve. ​Our Mission is to change a culture.</p>
+      <p>The ​plan ​is to educate families, professionals and communities through the sharing of knowledge and understanding.</p>
+      <p>We envision a future in which, no child with even the smallest potential to walk, is deprived of that gift by accident of situation, circumstance or ​ignorance.</p>
     </div>
   )
+}
+
+const pstyles = {
+  marginLeft: '20%'
 }
